0e9f27bdf5e643e8065a181f719f359ef636b6fe
pahmel0 <mantasx5@gmail.com>
2025-01-28 14:27:20 +0100

    Added a new endpoint for /hei with status code 301 (Moved Permanently)

--- example-01-HelloSpring/src/main/java/no/ntnu/hello/GreetingController.java
+++ example-01-HelloSpring/src/main/java/no/ntnu/hello/GreetingController.java
@@ -2,6 +2,8 @@ package no.ntnu.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 /**
  * A REST API controller which responds to HTTP requests for /hello.
@@ -18,5 +20,19 @@ public class GreetingController {
   public String greeting() {
     return "Hello, World!";
   }
+
+  /**
+   * Responds to HTTP GET requests for /hei with a permanent redirect.
+   *
+   * @return a ResponseEntity with 301 status code and message
+   */
+  @GetMapping("/hei")
+  public ResponseEntity<String> norwegianGreeting() {
+    return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
+            .body("Use /hello");
+  }
+  
 }
+
+
 
